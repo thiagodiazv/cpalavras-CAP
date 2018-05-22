5f2e189225d03e1c3096f93c737fcb5a97ec22f6
brunomengaldo <39499843+brunomengaldo@users.noreply.github.com>
2018-05-22 13:08:27 -0300

    Add files via upload

--- entrada.c
+++ entrada.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-#define max 10
+//prototipos
+void start();
 
+//main
 int main(){
 
-    int start;
-    
-    printf("Escolha o modo de criacao do jogo:\n\n");
-    printf("\t - Matriz e palavras aleatorias (0)\n");
-    printf("\t - Inserir tudo manualmente (1)\n");
-    printf("\t - Abrir arquivos com matriz e palavras (2)\n");
-    printf("\t - Matriz aleatoria e arquivo com palavras (3)\n");
-    printf("\t - Arquivo com matriz e palavras aleatorias (4)\n");    
-    printf("\n");
-    scanf("%d", &start);
-    printf("Voce escolheu opcao ~%d~", start);
+    start();
 
     return 0;
+}
+
+
+//documentacao
+
+void start(){
+
+    int opcao;
+    
+    printf("Escolha o modo de criacao do caca palavras:\n\n");
+    printf("\t - Abrir arquivos com matriz e palavras (0)\n");
+    printf("\t - Matriz e palavras aleatorias (1)\n");
+    printf("\t - Inserir tudo manualmente (2)\n");
+
+    scanf("%d", &opcao);
+    printf("\nVoce escolheu opcao ~%d~", opcao);
+
 }
